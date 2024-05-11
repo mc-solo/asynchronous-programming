8def50f8531d80cfe45ae198bb277f870704d969
wondwoesn <revatoel@gmail.com>
2024-05-11 23:27:37 +0300

    a bit about thenables

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// fetch API requires a discussion of callbacks, Thenables and Async/Await
 // callbacks are outdated so we use promises
 // callbacks can result in a callback hell
 
@@ -12,3 +13,24 @@ const myPromise = new Promise((resolve, reject) => {
     reject("No! rejected");
   }
 });
+
+// if we console log the promise, we gonna get the state of the promise and not the value
+
+// A thenable is an object that has a special function called then().
+// This function is like a notification - it tells your program what to do next
+
+const prom = new Promise((resolve, reject) => {
+  // stimulating async operation
+  setTimeout(() => {
+    resolve("Promise resolved");
+  }, 2000);
+});
+
+prom.then(
+  (value) => {
+    console.log(value);
+  },
+  (error) => {
+    console.error(error);
+  }
+);

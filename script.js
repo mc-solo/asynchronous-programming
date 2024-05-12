640c57f4b2341e6c25fb06b6b0b41a6fe704f191
wondwoesn <revatoel@gmail.com>
2024-05-12 12:34:01 +0300

    the fetch API

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-// fetch API requires a discussion of callbacks, Thenables and Async/Await
-// callbacks are outdated so we use promises
-// callbacks can result in a callback hell
+// // fetch API requires a discussion of callbacks, Thenables and Async/Await
+// // callbacks are outdated so we use promises
+// // callbacks can result in a callback hell
 
-// Promises: 3 states: fulfilled, rejected or pending
+// // Promises: 3 states: fulfilled, rejected or pending
 
-const myPromise = new Promise((resolve, reject) => {
-  const error = false;
+// const myPromise = new Promise((resolve, reject) => {
+//   const error = false;
 
-  if (!error) {
-    resolve("Yes! resolved.");
-  } else {
-    reject("No! rejected");
-  }
-});
+//   if (!error) {
+//     resolve("Yes! resolved.");
+//   } else {
+//     reject("No! rejected");
+//   }
+// });
 
-// if we console log the promise, we gonna get the state of the promise and not the value
+// // if we console log the promise, we gonna get the state of the promise and not the value
 
-// A thenable is an object that has a special function called then().
-// This function is like a notification - it tells your program what to do next
+// // A thenable is an object that has a special function called then().
+// // This function is like a notification - it tells your program what to do next
 
-const prom = new Promise((resolve, reject) => {
-  // stimulating async operation
-  setTimeout(() => {
-    resolve("Promise resolved");
-  }, 2000);
-});
+// const prom = new Promise((resolve, reject) => {
+//   // stimulating async operation
+//   setTimeout(() => {
+//     resolve("Promise resolved");
+//   }, 2000);
+// });
 
-prom.then(
-  (value) => {
-    console.log(value);
-  },
-  (error) => {
-    console.error(error);
-  }
-);
+// prom.then(
+//   (value) => {
+//     console.log(value);
+//   },
+//   (error) => {
+//     console.error(error);
+//   }
+// );
+
+const users = fetch("https://jsonplaceholder.typicode.com/users");
+
+// pending, i aint gonna use this if i want to work with the promise response
+console.log(users);
